ab36c8becebe6f22e2a5a00ad0fbf5119352bcd0
Oleksandr Mul <mulalex834@gmail.com>
2021-12-05 20:09:41 +0100

    Styling the weather card

--- src/components/weatherDetails.jsx
+++ src/components/weatherDetails.jsx
@@ -13,8 +13,55 @@ function WeatherDetails() {
           </div>
           <div className="description">
             <div className="weatherCondition">sunny</div>
-            <div className="place">
-                Berlin, DE
+            <div className="place">Berlin, DE</div>
+          </div>
+        </div>
+        <div className="date">{new Date().toLocaleString()}</div>
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"} />
+              </p>
+              <p className="extra-info-leftside">
+                4:30 PM <br />
+                Sunset
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"} />
+              </p>
+              <p className="extra-info-leftside">
+                50%
+                <br />
+                Humidity
+              </p>
+            </div>
+          </div>
+
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-rain"} />
+              </p>
+              <p className="extra-info-leftside">
+                67%
+                <br />
+                Rain
+              </p>
+            </div>
+            
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-strong-wind"} />
+              </p>
+              <p className="extra-info-leftside">
+                22 m/s
+                <br />
+                Wind
+              </p>
             </div>
           </div>
         </div>
